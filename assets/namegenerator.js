72ec742dbb7cf12ceed638dca48cb3b79fb73efa
pryor74 <thomaspryor@btconnect.com>
2018-11-30 16:21:39 +0000

    Update namegenerator.js
    Adding remove functionality

--- assets/namegenerator.js
+++ assets/namegenerator.js
@@ -6,6 +6,18 @@ $(".submit").on("click", function(){
   let addName = $(".input").val();
   if (addName.length > 0 && $.inArray(addName, names) == -1) {
     names.push(addName);
-    NAMESLIST.append("<li>" + addName + DELETENAME + "</li>");
+    NAMESLIST.append('<li data-name="addName">' + addName + DELETENAME + '</li>');
   }
+  console.log(names);
+});
+
+$(".remove-name").on("click", function(){
+  let $parent = $(this).parent();
+  let removeName = $parent.data("name");
+  let index = names.indexOf(removeName);
+  if (index > -1) {
+    names.splice(index, 1);
+  }
+  $(this).parent().remove();
+  console.log(names);
 });
