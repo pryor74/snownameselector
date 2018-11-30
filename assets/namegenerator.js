b324eff0e64583a29ee6f8adecd3efb4ea62d7f5
pryor74 <thomaspryor@btconnect.com>
2018-11-30 16:12:54 +0000

    Update namegenerator.js

--- assets/namegenerator.js
+++ assets/namegenerator.js
@@ -1,10 +1,11 @@
 const NAMESLIST = $("#names-list");
+const DELETENAME = "<i class=""fas fa-times-circle remove-name""></i>";
 let names = [];
 
 $(".submit").on("click", function(){
   let addName = $(".input").val();
   if (addName.length > 0 && $.inArray(addName, names) == -1) {
     names.push(addName);
-    NAMESLIST.append("<li>" + addName + "</li>");
+    NAMESLIST.append("<li>" + addName + DELETENAME + "</li>");
   }
 });
