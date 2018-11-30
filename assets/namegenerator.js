02b0a9eeaaf17eada9d8b493f08eaaa64541b6c2
pryor74 <thomaspryor@btconnect.com>
2018-11-30 15:36:47 +0000

    Update namegenerator.js

--- assets/namegenerator.js
+++ assets/namegenerator.js
@@ -2,7 +2,7 @@ const NAMESLIST = $("#names-list");
 let names = [];
 
 $(".submit").on("click", function(){
-  let addName = $(this).val();
+  let addName = $(".input").val();
   if (addName.length > 0 && $.inArray(addName, names) == -1) {
     names.push(addName);
     NAMESLIST.append("<li>" + addName + "</li>");
