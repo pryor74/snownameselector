67f8d7e755ae461931bf8676841bb247e2e96373
pryor74 <thomaspryor@btconnect.com>
2018-11-30 17:08:55 +0000

    Update namegenerator.js

--- assets/namegenerator.js
+++ assets/namegenerator.js
@@ -8,6 +8,25 @@ function randomIntFromInterval(min,max) // min and max included
     return Math.floor(Math.random()*(max-min+1)+min);
 }
 
+function displayWinner(){
+    if (names.length > 0) {
+        WINNER.html("");
+        let winnerIndex = randomIntFromInterval(0, (names.length - 1));
+        let winner  = names[winnerIndex];
+        var winnerElements = winner.split("").map(function(c) {
+            return $('<span id="' + c + '">' + c + '</span>');
+        });
+        var delay = 250; // Tune this for different letter delays.
+        winnerElements.forEach(function(e, i) {
+            WINNER.append(e);
+            e.hide();
+            setTimeout(function() {
+                e.fadeIn(300)
+            }, 100 + i * delay)
+        })
+    }  
+}
+
 $(".submit").on("click", function(){
   let addName = $(".input").val();
   if (addName.length > 0 && $.inArray(addName, names) == -1) {
@@ -29,19 +48,5 @@ $(document).on("click", ".remove-name", function(){
 });
 
 $(".generate").on("click", function(){
-    if (names.length > 0) {
-        let winnerIndex = randomIntFromInterval(0, (names.length - 1));
-        let winner  = names[winnerIndex];
-        var winnerElements = winner.split("").map(function(c) {
-            return $('<span id="' + c + '">' + c + '</span>');
-        });
-        var delay = 50; // Tune this for different letter delays.
-        winnerElements.forEach(function(e, i) {
-            WINNER.append(e);
-            e.hide();
-            setTimeout(function() {
-                e.fadeIn(300)
-            }, 100 + i * delay)
-        })
-    }  
+    displayWinner();
 });
